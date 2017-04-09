d805778dcccb187d96b94a5cf5c13e9ef2c0e24e
Romain Zunino <zunino.rom@gmail.com>
2017-04-09 05:49:27 -0700

    add properties for car object

--- MyCar.py
+++ MyCar.py
@@ -3,9 +3,13 @@ class car:
         self.color=''
         self.model=''
         self.maxSpeed=0
-
+        self.insurancePrice=0
+        self.wheelSize=0
+        
     def accelerate(self):
         return 'wroum wroum'
 
     def brake(self):
         return 'Stoppppp'
+
+
